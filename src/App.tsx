eab68b960c1f543803a5caa2f1d7d9ec9e1dd5b3
curious33 <curious.startpro@gmail.com>
2022-08-10 15:03:43 +0500

    add: routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const HomeView = lazy(() => import("views/home"));
+
 function App() {
-  return <>Hello world</>;
+  return (
+    <Router>
+      <Suspense fallback="Loading...">
+        <Routes>
+          <Route index element={<HomeView />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
