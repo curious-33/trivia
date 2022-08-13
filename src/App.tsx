02adf7372af58b58f34ea4d6142040f7db248e6c
curious33 <curious.startpro@gmail.com>
2022-08-14 00:06:14 +0500

    add: error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import {
   Navigate,
 } from "react-router-dom";
 
+import Layout from "./layout";
+
 const HomeView = lazy(() => import("views/home"));
 const QuizView = lazy(() => import("views/quiz"));
 const ResultView = lazy(() => import("views/result"));
@@ -13,14 +15,16 @@ const ResultView = lazy(() => import("views/result"));
 function App() {
   return (
     <Router>
-      <Suspense fallback="Loading...">
-        <Routes>
-          <Route path="/quizzes" element={<HomeView />} />
-          <Route path="/quizzes/:id" element={<QuizView />} />
-          <Route path="/quizzes/result" element={<ResultView />} />
-          <Route path="*" element={<Navigate to="/quizzes" replace />} />
-        </Routes>
-      </Suspense>
+      <Layout.Error>
+        <Suspense fallback="Loading...">
+          <Routes>
+            <Route path="/quizzes" element={<HomeView />} />
+            <Route path="/quizzes/:id" element={<QuizView />} />
+            <Route path="/quizzes/result" element={<ResultView />} />
+            <Route path="*" element={<Navigate to="/quizzes" replace />} />
+          </Routes>
+        </Suspense>
+      </Layout.Error>
     </Router>
   );
 }
--- src/layout/Error/index.tsx
+++ src/layout/Error/index.tsx
@@ -1,40 +1,48 @@
-// import { ErrorBoundary } from "react-error-boundary";
+import { ErrorBoundary } from "react-error-boundary";
 
-// import "./error.module.scss";
+import { Button } from "components";
+import styles from "./error.module.scss";
 
-// interface IError {
-//   message: string;
-// }
+interface IError {
+  message: string;
+}
 
-// interface IErrorFallBack {
-//   error: IError;
-//   resetErrorBoundary: Function;
-// }
+interface IErrorFallBack {
+  error: IError;
+  resetErrorBoundary: Function;
+}
 
-// function ErrorFallback({ error, resetErrorBoundary }) {
-//   return (
-//     <div className="errorBoundary" role="alert">
-//       <h1 className="errorBoundary-title">something_went_wrong:</h1>
-//       <p className="errorBoundary-message">error_occured: {error.message}</p>
-//       <button>Go home</button>
-//     </div>
-//   );
-// }
+function ErrorFallback({ error, resetErrorBoundary }: IErrorFallBack) {
+  return (
+    <div className={styles.errorBoundary} role="alert">
+      <h1 className={styles.errorBoundaryTitle}>Something went wrong</h1>
+      <p className={styles.errorBoundaryMessage}>
+        Error occured: {error.message}
+      </p>
+      <Button
+        name="Go home"
+        variant="orange"
+        onClick={() => {
+          window.location.href = "/";
+        }}
+      />
+    </div>
+  );
+}
 
-// const UI = ({ children }) => (
-//   <ErrorBoundary
-//     FallbackComponent={ErrorFallback}
-//     onReset={() => {
-//       // reset the state of your app so the error doesn't happen again
-//     }}
-//     onError={(error, componentStack) => {
-//       console.log(error);
-//       console.log("Error has arised: ", componentStack);
-//     }}
-//   >
-//     {children}
-//   </ErrorBoundary>
-// );
+const UI = ({ children }: { children: React.ReactNode }) => (
+  <ErrorBoundary
+    FallbackComponent={ErrorFallback}
+    onReset={() => {
+      // reset the state of your app so the error doesn't happen again
+    }}
+    onError={(error, componentStack) => {
+      console.log(error);
+      console.log("Error has arised: ", componentStack);
+    }}
+  >
+    {children}
+  </ErrorBoundary>
+);
 
-// export default UI;
-export {};
+export default UI;
