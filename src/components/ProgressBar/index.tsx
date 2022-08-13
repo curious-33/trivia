2d04e2f9989338acb73525105bfc250ff9dad7d7
curious33 <curious.startpro@gmail.com>
2022-08-13 22:15:09 +0500

    add: progres bar

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,8 +1,25 @@
 import styles from "./progressbar.module.scss";
 
-const ProgressBarComponent = () => {
+const ProgressBarComponent = ({ total, done }: { total: any; done: any }) => {
+  const _total = Array.isArray(total) ? total : [];
+  const _done = Array.isArray(done) ? done : [];
+
+  const totalLength = _total.length;
+  const doneLength = _done.length;
   return (
-    <div className={styles.progressBarComponent}>ProgressBarComponent</div>
+    <div className={styles.progressBarComponent}>
+      <div className={styles.progressText}>
+        <span>{doneLength}</span>/{totalLength}
+      </div>
+      <div className={styles.progress}>
+        <span
+          className={styles.progressCompleted}
+          style={{
+            width: `${Math.round((doneLength * 100) / totalLength)}%`,
+          }}
+        />
+      </div>
+    </div>
   );
 };
 
