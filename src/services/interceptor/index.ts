dd42ad3248b4978ea6692d9e5ad56e7862fc5a68
curious33 <curious.startpro@gmail.com>
2022-08-29 18:37:31 +0500

    add: handle base url

--- src/services/interceptor/index.ts
+++ src/services/interceptor/index.ts
@@ -1,8 +1,18 @@
 import axios, { AxiosRequestConfig, AxiosInstance } from "axios";
 import config from "config";
 
+function handleBaseUrl() {
+  if (config.api.base_url) {
+    return config.api.base_url;
+  } else {
+    throw new Error(
+      "No base url in env. Please, add base url to the env file. See the repo: https://github.com/curious-33/trivia"
+    );
+  }
+}
+
 const http: AxiosInstance = axios.create({
-  baseURL: config.api.base_url,
+  baseURL: handleBaseUrl(),
 });
 
 http.interceptors.request.use(
